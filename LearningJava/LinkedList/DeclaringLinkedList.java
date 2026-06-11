0597074a6ad455a93a52f5af954154f901ac8190
pamithkodekal <pamith.kodekal00@gmail.com>
2026-06-11 22:12:10 +0530

    LinkedList with manual nodes forming

---
+++ LearningJava/LinkedList/DeclaringLinkedList.java
@@ -0,0 +1,15 @@
+
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class DeclaringLinkedList{
+
+    public static void main(String[] args) {
+        LinkedList < Integer > L1 = new LinkedList<>();
+        L1.add(30);
+        L1.add(40);L1.add(50);L1.add(60);
+        System.out.print(L1);
+
+    }
+}
