cf301423786e55d64df76896fae072d93ab336dd
pamithkodekal <pamith.kodekal00@gmail.com>
2026-07-14 23:16:39 +0530

    Max sum using NaiveApproch

---
+++ Array/slidingWindow/maxSum.java
@@ -0,0 +1,23 @@
+package Array.slidingWindow;
+import Array.twoPointers.*;
+//import java.util.*;
+public class maxSum {
+    public static void main(String[] args) {
+  
+     int [] myArray = EnterArray.inputArray();
+     
+
+     int max = naiveApproach.Maxsum(myArray);
+     System.out.print(max);
+
+     
+        }
+        
+        
+    
+
+     
+
+
+
+    }
---
+++ Array/slidingWindow/naiveApproach.java
@@ -0,0 +1,30 @@
+package Array.slidingWindow;
+
+import java.util.*;
+
+public class naiveApproach {
+
+    public static int Maxsum(int[] arr) {
+
+        Scanner na = new Scanner(System.in);
+
+        System.out.println("Enter size of subarray (k): ");
+        int k = na.nextInt();
+
+        int max_sum = Integer.MIN_VALUE;
+
+        for (int i = 0; i <= arr.length - k; i++) {
+
+            int current_sum = 0;
+
+            for (int j = 0; j < k; j++) {
+                current_sum += arr[i + j];
+            }
+
+            max_sum = Math.max(current_sum, max_sum);
+        }
+        na.close();
+
+        return max_sum;
+    }
+}
